188d0bdd3eff768c6d279d7dec369bff9c615f53
lowyek <165703850+lowyek@users.noreply.github.com>
2025-01-09 00:22:37 +0530

    added backprop

--- src/gradops.c
+++ src/gradops.c
@@ -1,20 +1,26 @@
+#define MAX_PARAMS 100
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
 
 typedef struct Value {
-    double data;  
-    double grad;  
-    struct Value** _prev;  
-    size_t num_prev;  
-    char* op; 
-    void (*_backward)(struct Value*); 
+    double data;
+    double grad;
+    struct Value** _prev;
+    size_t num_prev;
+    char* op;
+    void (*_backward)(struct Value*);
+    size_t id;
 } Value;
+
+size_t node_counter = 0;
 
 void add_backward(Value* out);
 void mul_backward(Value* out);
 void relu_backward(Value* out);
+void noop_backward(Value* out);
 
 Value* create_value(double data, size_t num_prev, struct Value** prev, const char* op) {
     Value* v = (Value*)malloc(sizeof(Value));
@@ -23,25 +29,52 @@ Value* create_value(double data, size_t num_prev, struct Value** prev, const cha
     v->_prev = prev;
     v->num_prev = num_prev;
     v->op = op ? strdup(op) : NULL;
-    v->_backward = NULL;
+    v->_backward = noop_backward;
+    v->id = node_counter++;
     return v;
 }
 
+void noop_backward(Value* out) {
+    // nop
+}
 
 Value* add(Value* self, Value* other) {
-    Value* out = create_value(self->data + other->data, 2, (Value*[]){self, other}, "+");
+    Value** prev_nodes = (Value**)malloc(2 * sizeof(Value*));
+    if (prev_nodes == NULL) {
+        fprintf(stderr, "Memory allocation failed for prev_nodes\n");
+        return NULL;
+    }
+
+    prev_nodes[0] = self;
+    prev_nodes[1] = other;
+    Value* out = create_value(self->data + other->data, 2, prev_nodes, "+");
     out->_backward = add_backward;
     return out;
 }
 
+
 Value* mul(Value* self, Value* other) {
-    Value* out = create_value(self->data * other->data, 2, (Value*[]){self, other}, "*");
+    Value** prev_nodes = (Value**)malloc(2 * sizeof(Value*));
+    if (prev_nodes == NULL) {
+        fprintf(stderr, "Memory allocation failed for prev_nodes\n");
+        return NULL;
+    }
+    prev_nodes[0] = self;
+    prev_nodes[1] = other;
+    Value* out = create_value(self->data * other->data, 2, prev_nodes, "*");
     out->_backward = mul_backward;
     return out;
 }
 
 Value* relu(Value* self) {
-    Value* out = create_value(self->data > 0 ? self->data : 0, 1, (Value*[]){self}, "ReLU");
+    Value** prev_nodes = (Value**)malloc(sizeof(Value*));
+    if (prev_nodes == NULL) {
+        fprintf(stderr, "Memory allocation failed for prev_nodes\n");
+        return NULL;
+    }
+
+    prev_nodes[0] = self;
+    Value* out = create_value(self->data > 0 ? self->data : 0, 1, prev_nodes, "ReLU");
     out->_backward = relu_backward;
     return out;
 }
@@ -60,3 +93,41 @@ void relu_backward(Value* out) {
     out->_prev[0]->grad += (out->data > 0) * out->grad;
 }
 
+void build_topo(Value* v, Value** topo, int* idx, char* visited) {
+    if (!visited[v->id] && v!= NULL) {
+        visited[v->id] = 1;
+        for (size_t i = 0; i < v->num_prev; i++) {
+            build_topo(v->_prev[i], topo, idx, visited);
+        }
+        topo[*idx] = v;
+        (*idx)++;
+    }
+}
+
+void backward(Value* v) {
+    v->grad = 1.0;
+    
+    char visited[MAX_PARAMS] = {0};
+    Value* topo[MAX_PARAMS];
+    int idx = 0;
+    
+    build_topo(v, topo, &idx, visited);
+
+    for (int i = idx; i > 0; i--) {
+        topo[i - 1]->_backward(topo[i - 1]);
+    }
+}
+
+// int main() {
+//     Value* v1 = create_value(2.0, 0, NULL, "input");
+//     Value* v0 = create_value(2.0, 0, NULL, "input");
+//     Value* v3 = create_value(4.0,0,NULL,"input");
+
+
+//     Value* v2 = mul(v0, v1);
+//     Value* v4 = add(v3, v2);
+
+//     backward(v4);
+
+//     return 0;
+// }
