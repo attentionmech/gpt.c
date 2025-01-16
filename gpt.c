b2a76674343848bb73c631e7689018218a87ab2c
lowyek <165703850+lowyek@users.noreply.github.com>
2025-01-17 03:56:05 +0530

    not working due to vanishing gradient, but no segfaults

--- gpt.c
+++ gpt.c
@@ -4,7 +4,6 @@
 #include <time.h>
 #include <string.h>
 
-
 #define MAX_SLOTS 1000000
 #define IMAGE_SIZE 784
 
@@ -17,6 +16,10 @@ typedef enum
     SIGMOID,
     RELU,
     PARAMETER,
+    NEG,
+    EXP,
+    DIV,
+    LOG
 } OperationType;
 
 typedef struct
@@ -33,21 +36,34 @@ typedef struct
 Slot slots[MAX_SLOTS];
 int slot_count = 0;
 
-#include <stdio.h>
-
-// Helper function to map OperationType enum to strings
-const char* get_operation_name(OperationType op)
+const char *get_operation_name(OperationType op)
 {
     switch (op)
     {
-        case ADD:       return "ADD";
-        case MULTIPLY:  return "MULTIPLY";
-        case SUB:       return "SUB";
-        case POW2:      return "POW2";
-        case SIGMOID:   return "SIGMOID";
-        case RELU:      return "RELU";
-        case PARAMETER: return "PARAM";
-        default:        return "UNKNOWN";
+    case ADD:
+        return "ADD";
+    case MULTIPLY:
+        return "MULTIPLY";
+    case SUB:
+        return "SUB";
+    case POW2:
+        return "POW2";
+    case SIGMOID:
+        return "SIGMOID";
+    case RELU:
+        return "RELU";
+    case PARAMETER:
+        return "PARAM";
+    case EXP:
+        return "EXP";
+    case DIV:
+        return "DIV";
+    case LOG:
+        return "LOG";
+    case NEG:
+        return "NEG";
+    default:
+        return "UNKNOWN";
     }
 }
 
@@ -82,8 +98,7 @@ void export_graph_to_dot(const char *filename)
     printf("Graph exported to %s. Use 'dot -Tpng %s -o graph.png' to generate an image.\n", filename, filename);
 }
 
-
-
+// --------------------------------------------
 
 double random_init()
 {
@@ -116,7 +131,7 @@ int create_value_slot(double value, int learnable_param)
     return increment_slot();
 }
 
-int create_operation_slot(OperationType op, int* dep, int num_dependencies)
+int create_operation_slot(OperationType op, int *dep, int num_dependencies)
 {
     slots[slot_count].operation = op;
     slots[slot_count].dependencies = dep;
@@ -136,7 +151,6 @@ double get_value_slot(int slot)
 {
     return slots[slot].value;
 }
-
 
 double _sum(double *list, int length)
 {
@@ -161,19 +175,22 @@ double _mul(double *list, int length)
 double compute_graph(int slot)
 {
 
+    // printf("Computing value for slot %d\n", slot);
+
     Slot *s = &slots[slot];
 
-    if (s->visited){
+    if (s->visited)
+    {
         return s->value;
     }
 
     if (s->num_dependencies > 0)
     {
         double dep_value[s->num_dependencies];
-        for (int j=0; j< s->num_dependencies; j++){
+        for (int j = 0; j < s->num_dependencies; j++)
+        {
             dep_value[j] = compute_graph(s->dependencies[j]);
         }
-
 
         switch (s->operation)
         {
@@ -184,12 +201,26 @@ double compute_graph(int slot)
             s->value = _mul(dep_value, s->num_dependencies);
             break;
 
-
-         case SUB:
-            s->value = dep_value[0] - dep_value[1];  // a - b
+        case EXP:
+            s->value = exp(dep_value[0]);
             break;
-         case POW2:
-            s->value = dep_value[0] * dep_value[0];  // a^2
+        case DIV:
+            s->value = dep_value[0] / dep_value[1];
+            break;
+
+         case NEG:
+            s->value = -dep_value[0];
+            break;
+
+        case LOG:
+            s->value = log(dep_value[0]);
+            break;
+
+        case SUB:
+            s->value = dep_value[0] - dep_value[1]; 
+            break;
+        case POW2:
+            s->value = dep_value[0] * dep_value[0]; 
             break;
         case SIGMOID:
             s->value = 1.0 / (1.0 + exp(-dep_value[0]));
@@ -203,7 +234,7 @@ double compute_graph(int slot)
     }
 
     s->visited = 1;
-
+    // printf("Slot %d: Value = %.2f\n", slot, s->value);
     return s->value;
 }
 
@@ -241,16 +272,15 @@ void compute_grad(int slot)
                 slots[s->dependencies[i]].gradient += s->gradient * product;
             }
             break;
-        
-         case SUB:
-            slots[s->dependencies[0]].gradient += s->gradient;      
-            slots[s->dependencies[1]].gradient -= s->gradient; 
-            break;
-        
-         case POW2:
-            slots[s->dependencies[0]].gradient += s->gradient * 2.0 * dep_value[0];  
+
+        case SUB:
+            slots[s->dependencies[0]].gradient += s->gradient;
+            slots[s->dependencies[1]].gradient -= s->gradient;
             break;
 
+        case POW2:
+            slots[s->dependencies[0]].gradient += s->gradient * 2.0 * dep_value[0];
+            break;
 
         case SIGMOID:
             slots[s->dependencies[0]].gradient += s->gradient * s->value * (1.0 - s->value);
@@ -261,6 +291,23 @@ void compute_grad(int slot)
             {
                 slots[s->dependencies[0]].gradient += s->gradient;
             }
+            break;
+
+        case EXP:
+            slots[s->dependencies[0]].gradient += s->gradient * s->value;
+            break;
+
+        case NEG:
+            slots[s->dependencies[0]].gradient += s->gradient * -1.0;
+            break;
+
+        case DIV:
+            slots[s->dependencies[0]].gradient += s->gradient / dep_value[1];
+            slots[s->dependencies[1]].gradient -= s->gradient * dep_value[0] / (dep_value[1] * dep_value[1]);
+            break;
+
+        case LOG:
+            slots[s->dependencies[0]].gradient += s->gradient * (1.0 / dep_value[0]);
             break;
 
         default:
@@ -274,21 +321,22 @@ void compute_grad(int slot)
     }
 }
 
-int *wrap_value_in_array(int a){
-    int *arr = malloc(1*sizeof(int));
+int *wrap_value_in_array(int a)
+{
+    int *arr = malloc(1 * sizeof(int));
     arr[0] = a;
     return arr;
 }
 
-int *wrap_in_array(int a, int b){
-    int *arr = malloc(2*sizeof(int));
+int *wrap_in_array(int a, int b)
+{
+    int *arr = malloc(2 * sizeof(int));
     arr[0] = a;
     arr[1] = b;
     return arr;
 }
 
-
-int* create_feedforward_network(int *layer_sizes, int num_layers)
+int *create_feedforward_network(int *layer_sizes, int num_layers)
 {
     int *prev_layer_slots = NULL;
     int *curr_layer_slots = NULL;
@@ -313,7 +361,8 @@ int* create_feedforward_network(int *layer_sizes, int num_layers)
                 int sum = -1;
                 for (int prev = 0; prev < layer_sizes[layer - 1]; prev++)
                 {
-                    int weight = create_value_slot(0.1, 1);
+                    double random_value = (double)rand() / RAND_MAX;
+                    int weight = create_value_slot(random_value, 1);
                     int mul = create_operation_slot(MULTIPLY, wrap_in_array(prev_layer_slots[prev], weight), 2);
 
                     if (sum == -1)
@@ -327,7 +376,7 @@ int* create_feedforward_network(int *layer_sizes, int num_layers)
                 }
 
                 int bias = create_value_slot(0.0, 1);
-                int biased = create_operation_slot(ADD, wrap_in_array(sum, bias),2);
+                int biased = create_operation_slot(ADD, wrap_in_array(sum, bias), 2);
 
                 curr_layer_slots[neuron] = create_operation_slot(RELU, wrap_value_in_array(biased), 1);
             }
@@ -340,35 +389,85 @@ int* create_feedforward_network(int *layer_sizes, int num_layers)
     return curr_layer_slots;
 }
 
+int *create_softmax_layer(int *input_slots, int num_outputs)
+{
+    double max_val = -INFINITY;
+    double *input_values = malloc(num_outputs * sizeof(double));
+
+    for (int i = 0; i < num_outputs; i++)
+    {
+        input_values[i] = get_value_slot(input_slots[i]);
+        if (input_values[i] > max_val)
+        {
+            max_val = input_values[i];
+        }
+    }
+
+    double sum_exp = 0.0;
+    double *exp_values = malloc(num_outputs * sizeof(double));
+    for (int i = 0; i < num_outputs; i++)
+    {
+        exp_values[i] = exp(input_values[i] - max_val);
+        sum_exp += exp_values[i];
+    }
+
+
+    int *softmax_slots = malloc(num_outputs * sizeof(int));
+    for (int i = 0; i < num_outputs; i++)
+    {
+        softmax_slots[i] = create_operation_slot(DIV, wrap_in_array(exp_values[i], sum_exp), 2);
+    }
+
+    free(input_values);
+    free(exp_values);
+
+    return softmax_slots;
+}
+
+int create_cross_entropy_loss(int *target_slots, int *softmax_slots, int num_outputs)
+{
+    int *log_slots = malloc(num_outputs * sizeof(int));
+    int *product_slots = malloc(num_outputs * sizeof(int));
+
+    for (int i = 0; i < num_outputs; i++)
+    {
+        int log_softmax = create_operation_slot(LOG, wrap_value_in_array(softmax_slots[i]), 1);
+        log_slots[i] = log_softmax;
+        product_slots[i] = create_operation_slot(MULTIPLY, wrap_in_array(target_slots[i], log_softmax), 2);
+    }
+
+    int sum_cross_entropy = create_operation_slot(ADD, product_slots, num_outputs);
+    int neg_cross_entropy = create_operation_slot(NEG, wrap_value_in_array(sum_cross_entropy), 1);
+    return neg_cross_entropy;
+}
 
 void train(double inputs[][IMAGE_SIZE], int labels[], int num_samples, double learning_rate)
 
 {
 
-    int num_inputs = IMAGE_SIZE;
+    int num_inputs = 784;
     int num_outputs = 10;
 
+    int layer_sizes[] = {num_inputs, num_outputs}; // 784 inputs, 128 hidden, 10 output neurons
+    int num_layers = 2;
 
-    int layer_sizes[] = {num_inputs, 32,32, num_outputs}; // 784 inputs, 128 hidden, 10 output neurons
-    int num_layers = 4;
+    int target_slots[num_outputs];
+
 
     int *output_slots = create_feedforward_network(layer_sizes, num_layers);
-    int target_slots[num_outputs];
-    int diff_slots[num_outputs];
+    int *softmax_slots = create_softmax_layer(output_slots, num_outputs);
 
     for(int i=0; i< num_outputs; i++){
-        target_slots[i] = create_value_slot(0.0, 0);
-        int temp = create_operation_slot(SUB, wrap_in_array(target_slots[i], output_slots[i]),2);
-        diff_slots[i] = create_operation_slot(POW2, wrap_value_in_array(temp), 1);
+        target_slots[i] = create_value_slot(0,0);
     }
 
-    int loss_slot = create_operation_slot(ADD, diff_slots,num_outputs);
+    int loss_slot = create_cross_entropy_loss(target_slots, softmax_slots, num_outputs);
 
     srand(time(NULL));
 
     // export_graph_to_dot("test.dot");
 
-    for (int epoch = 0; epoch < 100; epoch++) // Reduced number of epochs
+    for (int epoch = 0; epoch < 1; epoch++) // Reduced number of epochs
     {
         double total_loss = 0.0;
 
@@ -380,26 +479,30 @@ void train(double inputs[][IMAGE_SIZE], int labels[], int num_samples, double le
                 slots[j].visited = 0;
             }
 
-            // set sample
+            
             for (int k = 0; k < num_inputs; k++)
             {
                 set_value_slot(k, inputs[i][k]);
             }
 
             // one hot
-            for (int l =1; l <= num_outputs; l++){
-                if (l == labels[i]){
-                    set_value_slot(target_slots[l-1], 1);
-                } else {
-                    set_value_slot(target_slots[l-1], 0);
+            for (int l = 1; l <= num_outputs; l++)
+            {
+                if (l == labels[i])
+                {
+                    set_value_slot(target_slots[l - 1], 1);
+                }
+                else
+                {
+                    set_value_slot(target_slots[l - 1], 0);
                 }
             }
 
             compute_graph(loss_slot);
-
+            
+            
             
             total_loss += get_value_slot(loss_slot);
-
 
             slots[loss_slot].gradient = 1.0;
             compute_grad(loss_slot);
@@ -408,6 +511,7 @@ void train(double inputs[][IMAGE_SIZE], int labels[], int num_samples, double le
             {
                 if (slots[j].learnable_param == 1)
                 {
+                    printf("Slot %d: Gradient = %.6f\n", j, slots[j].gradient);
                     set_value_slot(j, get_value_slot(j) - learning_rate * slots[j].gradient);
                 }
             }
@@ -426,24 +530,24 @@ int main()
         return 1;
     }
 
-    int num_samples = 50;
+    int num_samples = 1;
     double inputs[num_samples][IMAGE_SIZE];
     int labels[num_samples];
 
-    
-    int i=0;
+    int i = 0;
 
+    while (fscanf(file, "%d", &labels[i]) != EOF && i < num_samples)
+    {
 
-     while (fscanf(file, "%d", &labels[i]) != EOF && i < num_samples) {
-        
-        for (int j = 0; j < IMAGE_SIZE; j++) {
+        for (int j = 0; j < IMAGE_SIZE; j++)
+        {
 
-            if (fscanf(file, "%lf", &inputs[i][j]) != 1) {
+            if (fscanf(file, "%lf", &inputs[i][j]) != 1)
+            {
                 break;
             }
         }
         i++;
-        
     }
 
     fclose(file);
